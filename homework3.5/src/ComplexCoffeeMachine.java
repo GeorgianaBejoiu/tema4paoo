e9b330529ffa509b0c6018685f3f56347aaa4f71
GeorgianaBejoiu <bejoiu.roxana.r3i@student.ucv.ro>
2025-05-12 20:20:03 +0300

    Add files via upload

---
+++ homework3.5/src/ComplexCoffeeMachine.java
@@ -0,0 +1,6 @@
+public class ComplexCoffeeMachine implements CoffeMachine{
+    @Override
+    public void start() {
+        System.out.println("ComplexCoffeeMachine:  espresso, cappuccino, latte...");
+    }
+}
---
+++ homework3.5/src/SimpleCoffeeMachine.java
@@ -0,0 +1,6 @@
+public class SimpleCoffeeMachine implements CoffeMachine{
+    @Override
+    public void start() {
+        System.out.println("SimpleCoffeeMachine: cafea simplă");
+    }
+}
